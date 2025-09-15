3d7ddf0a422215f2999ded519c36a74c2ccb936e
irshath1022im <im.irshath1022@gmail.com>
2025-09-15 17:04:41 +0300

    navBarupdate

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,29 +1,43 @@
-<div x-data="{ open: false }" >
-    <nav class="bg-blue-500 flex justify-between list-none p-4 xs:text-[10px] sm:text-[12px] lg:text-base">
-        {{-- <a href="{{ route('cmHome') }}"><li class="p-2 border-b border-b-orange-300 bg-slate-400 text-white"></li></a> --}}
+<div x-data="{ open: false }"
+    class="bg-white flex justify-between items-center list-none p-4 "
+>
+
+    <div class="w-44  p-2">
+
+        <img src="/images/logo.jpg" />
+    </div>
+
+    <nav class="flex  justify-between items-center">
+        {{-- <a href="{{ route('cmHome') }}"><li class="p-2 border-b border-b-orange-300 bg-slate-400 text-blue"></li></a> --}}
         {{-- <a href="{{ route('admin_technician') }}">
-            <li class="p-2 hover:border hover:border-orange-300  text-white">TECHNICIAN</li></a> --}}
+            <li class="p-2 hover:border hover:border-orange-300  text-blue">TECHNICIAN</li></a> --}}
 
 
         <a href="{{ route('dashBoard') }}">
-            <li class="p-2 hover:border hover:border-orange-300   text-white">DASHBOARD</li></a>
+            <li class="p-2 hover:border hover:border-orange-300   text-blue">DASHBOARD</li></a>
 
         <a href="{{ route('admin_equipment') }}">
-            <li class="p-2 hover:border hover:border-orange-300   text-white">EQUIPMENT</li></a>
+            <li class="p-2 hover:border hover:border-orange-300   text-blue">EQUIPMENT</li></a>
 
         <a href="{{ route('admin_tags') }}">
-            <li class="p-2 hover:border hover:border-orange-300  text-white">EQUIPMENT TAGS</li></a>
+            <li class="p-2 hover:border hover:border-orange-300  text-blue">EQUIPMENT TAGS</li></a>
         <a href="{{ route('cmHome') }}">
-            <li class="p-2 hover:border hover:border-orange-300  text-white">CM</li></a>
+            <li class="p-2 hover:border hover:border-orange-300  text-blue">CM</li></a>
         <a href="{{ route('admin_meterial_request') }}">
-            <li class="p-2 hover:border hover:border-orange-300  text-white">MATERIAL REQUEST</li></a>
+            <li class="p-2 hover:border hover:border-orange-300  text-blue">MATERIAL REQUEST</li></a>
         <a href="{{ route('admin_batch_orders') }}">
-            <li class="p-2 hover:border hover:border-orange-300    text-white">BATCH ORDERS</li></a>
+            <li class="p-2 hover:border hover:border-orange-300    text-blue">BATCH ORDERS</li></a>
         <a href="{{ route('admin_spare_parts') }}">
-            <li class="p-2 hover:border hover:border-orange-300  border-b-orange-300  text-white">SPARE PARTS</li>
+            <li class="p-2 hover:border hover:border-orange-300  border-b-orange-300  text-blue">SPARE PARTS</li>
         </a>
+        @guest
+
+            <a href="{{ route('login') }}">
+                <x-button class="btn btn-submit">LOGIN</x-button>
+            </a>
+        @endguest
         {{-- <a href="">
-            <li class="p-2 hover:border hover:border-orange-300    text-white">SERVICE DES</li></a> --}}
+            <li class="p-2 hover:border hover:border-orange-300    text-blue">SERVICE DES</li></a> --}}
 
 
             @auth
@@ -67,6 +81,36 @@
      @endauth
     </nav>
 
+
+    {{-- MOBILE MENU FOR SMALL SCREEN --}}
+{{--
+    <div class="bg-blue-500 xs:text-[10px] p-2 sm:hidden ">
+
+    <div class="p-2 cursor-pointer" x-on:click="open = ! open">
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+        </svg>
+
+    </div>
+
+    <div class=" relative">
+
+        <div class="bg-white absolute z-20 w-full h-screen top-0 -left-2"  x-show="open">
+                <ul class="divide-y-2">
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">DASHBOARD</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">EQUIPMENT</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">EQUIPMENT TAGS</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">CM</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">MATERIAL REQUEST</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">BADGE ORDER</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">SPARE PARTS</li>
+                    <li class="p-3 text-[14px] text-blue-800 font-bold">LOGIN</li>
+                </ul>
+        </div>
+    </div>
+
+
+    </div> --}}
 
 
 </div>
